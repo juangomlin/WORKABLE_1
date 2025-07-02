517fcec395269f87e9cb8f427d4d099692e72f55
daviidp <daviidap07@gmail.com>
2025-07-02 12:22:51 -0500

    correccion general estilos

---
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const ForgotPassword = () => {
+    <>
+        <Header />
+        <main>
+            <h1></h1>
+            <section></section>
+        </main>
+    </>
+}
