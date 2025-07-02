3569ca1de775d54f92839f649dd019a9befc9626
daviidp <daviidap07@gmail.com>
2025-07-02 13:43:43 -0500

    ForgotPsw page

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Header from "../Header/Header";
+import './ForgotPassword.css'
 
 const ForgotPassword = () => {
+    return (
     <>
         <Header />
-        <main>
-            <h1></h1>
-            <section></section>
-        </main>
+            <main className="main-forgotpsw">
+                <section className="section-forgotpsw">
+                    <h1>¿Olvidaste tu contraseña?</h1>
+                    <div className="div-forgotpsw">
+                        <form className="form-forgotpsw">
+                            <input type="text" 
+                            className="input-forgotpsw" 
+                            placeholder="Correo electronico"
+                            />
+                        </form>
+                    </div>
+                </section>
+            </main>
     </>
+    )
 }
+
+export default ForgotPassword;
